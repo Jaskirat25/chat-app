e1c678ac0909c40b4e2960c9d214e564e17237a2
Jaskirat25 <jaskiratsingh251103@gmail.com>
2025-08-02 20:29:51 +0530

    basic backend setup

--- client/src/pages/Loginpage.jsx
+++ client/src/pages/Loginpage.jsx
@@ -7,8 +7,15 @@ const [fullName, setFullName] = useState("")
 const [email, setEmail] = useState("")
 const [password, setPassword] = useState("")
 const [bio, setBio] = useState("")
-const [isDataSubmitted, setIsDataSubmitted] = useState(false);
+const [isDataSubmitted, setIsDataSubmitted] = useState(true);
 
+const onSubmitHandler=(event)=>{
+ event.preventDefault();
+ if(currState=="Sign up" && isDataSubmitted){
+    setIsDataSubmitted(true);
+    return;
+ }
+}
 return (
 <div className='min-h-screen bg-cover bg-center flex items-center
 justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
@@ -17,12 +24,11 @@ justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
 <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]'/>
 
 {/* right */}
-<form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex
-flex-col gap-6 rounded-lg shadow-lg'>
-<h2 className='font-medium text-2xl flex justify-between items-center'>
-{currState}
-<img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>
-</h2>
+<form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+  <h2 className='font-medium text-2xl flex justify-between items-center'>
+    {currState}
+    {isDataSubmitted && <img onClick={() => setIsDataSubmitted(false)} src={assets.arrow_icon} alt="" className='w-5 cursor-pointer' />}
+  </h2>
 
 {currState === "Sign up" && !isDataSubmitted && (
 <input onChange={(e)=>setFullName(e.target.value)} value={fullName} type="text" className='p-2 border border-gray-500 rounded-md
--- client/src/pages/Profilepage.jsx
+++ client/src/pages/Profilepage.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import assets from "../assets/assets";
 
 const Profilepage = () => {
-  return (
-    <div>Profilepage</div>
-  )
-}
+  const [selectedImg, setSelectedImg] = useState(null);
+  const navigate = useNavigate();
+  const [name, setName] = useState("Jassi");
+  const [bio, setBio] = useState("i am a lazy person");
 
-export default Profilepage
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
+  return (
+    <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
+      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+        <form onSubmit={handlesubmit} className="flex flex-col gap-5 p-10 flex-1 items-center">
+          <h3 className="text-lg self-start">Profile details</h3>
+
+          <label htmlFor="avatar" className="flex flex-col items-center gap-2 cursor-pointer">
+            <input
+              onChange={(e) => setSelectedImg(e.target.files[0])}
+              type="file"
+              id="avatar"
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+            <img
+              src={
+                selectedImg
+                  ? URL.createObjectURL(selectedImg)
+                  : assets.avatar_icon
+              }
+              alt=""
+              className="w-16 h-16 rounded-full"
+            />
+            {/* <span className="text-sm">upload profile image</span> */}
+          </label>
+
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            required
+            placeholder="Your name"
+            className="w-full p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+          />
+          <textarea
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+            placeholder="Write profile bio"
+            required
+            className="w-full p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+            rows={4}
+          />
+
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-purple-400 to-violet-600 text-white px-6 py-2 rounded-full text-lg cursor-pointer w-full"
+          >
+            Save
+          </button>
+        </form>
+
+        <img
+          className="max-w-50 aspect-square rounded-full mx-10 max-sm:mt-10"
+          src={assets.logo_icon}
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Profilepage;
