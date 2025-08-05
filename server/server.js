3412b2f40791bef00b860f54e335b5d65aaedbef
Jaskirat25 <jaskiratsingh251103@gmail.com>
2025-08-06 00:15:42 +0530

    backend complete

--- server/server.js
+++ server/server.js
@@ -3,15 +3,43 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/mongoose.js";
+import userRouter from "./routes/UserRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 // Create Express app and HTTP server
 const app = express();
 const server = http.createServer(app);
+
+export const io=new Server(server,{
+    cors:{origin:'*'}
+}) 
+
+export const userSocketMap={};
+
+io.on("connection",(socket)=>{
+const userId=socket.handshake.query.userId;
+console.log("user connected");
+if(userId){
+    userSocketMap[userId]=socket.id;
+}
+io.emit('getOnlineUsers',Object.keys(userSocketMap))
+
+socket.on('disconnect',()=>{
+    console.log("user disconnected");
+    delete userSocketMap[userId];
+    io.emit('getOnlineUsers',Object.keys(userSocketMap))
+
+    
+})
+
+})
 
 // Middleware setup
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("api/auth",userRouter);
+app.use("api/messages",messageRouter)
 await connectDB();
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
